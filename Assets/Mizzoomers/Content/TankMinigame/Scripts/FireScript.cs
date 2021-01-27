13fd8a706bd23f8a29f2f1b5a1d05f11d22f2d8e
NotSoUrbanSniper2 <jrs374@umsystem.edu>
2021-01-27 17:43:35 -0600

    Added Projectile to tank
    Added the first iteration of the tank firing it main gun

---
+++ Assets/Mizzoomers/Content/TankMinigame/Scripts/FireScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject FiringTank;
+    public Transform EndOfBarrel;
+    //public Transform EndOfBarrel2;
+    public GameObject Projectile;
+    public GameObject MuzzelExplosion;
+    public float ProjectileSpeed;
+    private bool canFire;
+    private bool timerRunning;
+    public float fireTime;
+    private float timeLeft;
+    
+    void Start()
+    {
+        canFire = true;
+        timerRunning = false;
+        timeLeft = 0;
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space") && canFire == true)
+        {
+            GameObject clone;
+            GameObject flash;
+            clone = Instantiate(Projectile, EndOfBarrel.position, EndOfBarrel.rotation);
+            flash = Instantiate(MuzzelExplosion, EndOfBarrel.position, EndOfBarrel.rotation);
+            ProjectileScript Projectilescript = clone.GetComponent<ProjectileScript>();
+            Projectilescript.firingTank = FiringTank;
+            Projectilescript.MuzzelFlash = flash;
+            Rigidbody clonebody = clone.GetComponent<Rigidbody>();
+            clonebody.velocity = EndOfBarrel.TransformDirection(Vector3.forward * ProjectileSpeed);
+            
+
+            timerRunning = true;
+            timeLeft = fireTime;
+            canFire = false;
+        }
+        if(timerRunning == true)
+        {
+            timeLeft -= Time.deltaTime;
+            if(timeLeft<= 0)
+            {
+                canFire = true;
+                timerRunning = false;
+                
+            }
+        }
+    }
+
+}
---
+++ Assets/Mizzoomers/Content/TankMinigame/Scripts/ProjectileScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject firingTank;
+    public GameObject Explosion;
+    public GameObject MuzzelFlash;
+    private float Timeout;
+    
+    void Start()
+    {
+        Timeout = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timeout -= Time.deltaTime;
+        if (Timeout <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject != firingTank && collision.gameObject != MuzzelFlash)
+        {
+            GameObject explode;
+            explode = Instantiate(Explosion,transform.position, transform.rotation);
+            //damage tank
+            Destroy(gameObject);
+        }
+        
+
+    }
+}
