dfbda93131c22453720c079f1100a2a99d07d5c7
NotSoUrbanSniper2 <jrs374@umsystem.edu>
2021-01-27 16:02:43 -0600

    Added Already Built Stuff
    Added stuff i built over winter break

---
+++ Assets/Mizzoomers/Content/TankMinigame/Scripts/BarrelMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelMovement : MonoBehaviour {
+	public float turnSpeed;
+	private float Temp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	//z cannot be greater than 5 or less than -13
+	// Update is called once per frame
+	void Update () {
+		// between 16 and  340
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			//print (transform.localEulerAngles.x);
+			if (transform.localEulerAngles.x <= 18 || transform.localEulerAngles.x > 340) {
+				transform.Rotate (-turnSpeed * Time.deltaTime,0.0f,0.0f);
+			}
+		}
+
+				
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			//print (transform.localEulerAngles.x);
+			if (transform.localEulerAngles.x < 16 || transform.localEulerAngles.x >= 330) {
+				transform.Rotate (turnSpeed * Time.deltaTime, 0.0f, 0.0f);
+			}
+		}
+
+		Temp = transform.localEulerAngles.z;
+				
+	}
+}
---
+++ Assets/Mizzoomers/Content/TankMinigame/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	public float moveSpeed;
+	public float turnSpeed;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A))
+			transform.Rotate (0.0f,-turnSpeed * Time.deltaTime,0.0f);
+		if (Input.GetKey (KeyCode.D))
+			transform.Rotate (0.0f, turnSpeed * Time.deltaTime,0.0f);
+		if (Input.GetKey (KeyCode.W))
+			transform.Translate ( 0.0f, 0.0f, moveSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.S))
+			transform.Translate ( 0.0f, 0.0f, -moveSpeed * Time.deltaTime);
+		
+	}
+}
