bee64b295fc1dfd2295379680a74e6158f0685e2
NotSoUrbanSniper2 <jrs374@umsystem.edu>
2021-02-07 18:33:25 -0600

    Added HP UI
    Added simple HP UI and a script to modify it.

---
+++ Assets/Mizzoomers/Content/TankMinigame/Scripts/ModifyHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ModifyHealth : MonoBehaviour
+{
+	public Text HealthText;
+	private int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void ChangeHealth(int change)
+	{
+		health = health + change;
+		HealthText.text = "HP: " + health.ToString();
+	}
+}
