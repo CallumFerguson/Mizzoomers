5fa0778a52e96a2984a84f4c955d145a3ae4cb7d
Callum <Calxferg@gmail.com>
2021-04-03 22:22:13 -0400

    basic graph algorithm

--- Assets/Mizzoomers/Content/HighGround/Scripts/GenerateWorld.cs
+++ Assets/Mizzoomers/Content/HighGround/Scripts/GenerateWorld.cs
@@ -37,20 +37,106 @@ public class GenerateWorld : NetworkBehaviour
 
         Random.InitState(_seed);
 
-        for (int i = 0; i < 500; i++)
+        var nodes = new List<Node>();
+
+        for (int i = 0; i < 15; i++)
         {
-            CreatePlatform(new Vector3(Random.value * 100, Random.value, Random.value * 100), new Vector2(Random.value * 5 + 1, Random.value * 5 + 1));
+            const float size = 35f;
+            const float space = 7.5f;
+
+
+            Vector2 pos = Vector2.zero;
+            int attempts = 10;
+            do
+            {
+                attempts--;
+                if (attempts == -1)
+                {
+                    break;
+                }
+                var x = Random.value * size;
+                var z = Random.value * size;
+                pos = new Vector2(x, z);
+            } while (PosNearAnother(pos, nodes, space));
+
+            if (attempts >= 0)
+            {
+                Node node;
+                node.pos = new Vector3(pos.x, Random.value * 5, pos.y);
+                node.rotation = Quaternion.identity;
+                node.scale = new Vector2(Random.value * 5 + 1, Random.value * 5 + 1);
+                nodes.Add(node);
+            }
+        }
+        
+        print($"Created {nodes.Count} nodes.");
+
+        var edges = new HashSet<(int, int)>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            var i1 = Random.Range(0, nodes.Count);
+            var i2 = Random.Range(0, nodes.Count);
+            if (i1 != i2 && !edges.Contains((i1, i2)))
+            {
+                edges.Add((i1, i2));
+            }
+        }
+        
+        print($"Created {edges.Count} edges.");
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            CreatePlatform(nodes[i]);
+        }
+
+        foreach (var edge in edges)
+        {
+            var n1 = nodes[edge.Item1];
+            var n2 = nodes[edge.Item2];
+            var p1 = n1.pos + new Vector3(0, 5 - 0.251f, 0);
+            var p2 = n2.pos + new Vector3(0, 5 - 0.251f, 0);
+            CreateBridge(p1, p2);
         }
     }
 
-    private void CreatePlatform(Vector3 position, Vector2 size)
+    private bool PosNearAnother(Vector2 pos, List<Node> nodes, float space)
     {
-        float height = position.y + 10 + 5;
+        for (int n = 0; n < nodes.Count; n++)
+        {
+            var nodePos = new Vector2(nodes[n].pos.x, nodes[n].pos.z);
+            if (Vector2.Distance(pos, nodePos) < space)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void CreatePlatform(Node node)
+    {
+        float height = node.pos.y + 10 + 5;
 
         var block = Instantiate(blockPrefab, transform);
 
-        block.transform.position = new Vector3(position.x, height / 2f - 10, position.z);
-        block.transform.rotation = Quaternion.identity;
-        block.transform.localScale = new Vector3(size.x, height, size.y);
+        block.transform.position = new Vector3(node.pos.x, height / 2f - 10, node.pos.z);
+        block.transform.rotation = Quaternion.AngleAxis(Random.value * 360f, Vector3.up);
+        block.transform.localScale = new Vector3(node.scale.x, height, node.scale.y);
+    }
+
+    private void CreateBridge(Vector3 p1, Vector3 p2)
+    {
+        var block = Instantiate(blockPrefab, transform);
+        block.transform.position = (p1 + p2) / 2f;
+        block.transform.LookAt(p2);
+        block.transform.localScale = new Vector3(1.5f, 0.5f, Vector3.Distance(p1, p2));
+    }
+
+    private struct Node
+    {
+        public Vector3 pos;
+        public Quaternion rotation;
+        public Vector2 scale;
     }
 }
