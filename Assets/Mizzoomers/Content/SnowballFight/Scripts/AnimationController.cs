d7622117d9db82b907e231c47dd2b91730a45ddf
Callum <Calxferg@gmail.com>
2021-04-06 14:52:19 -0400

    change a bunch of stuff

--- Assets/Mizzoomers/Content/SnowballFight/Scripts/AnimationController.cs
+++ Assets/Mizzoomers/Content/SnowballFight/Scripts/AnimationController.cs
@@ -6,18 +6,19 @@ public class AnimationController : MonoBehaviour
 {
     Animator animator;
     int isRunningHash;
-    int isJumpingHash;
+    // int isJumpingHash;
     int inAirHash;
     int throwHash;
 
+    public ThirdPersonMovement controller;
+    public Transform groundCheck;
     
-
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         isRunningHash = Animator.StringToHash("isRunning");
-        isJumpingHash = Animator.StringToHash("isJumping");
+        // isJumpingHash = Animator.StringToHash("isJumping");
         inAirHash = Animator.StringToHash("inAir");
         throwHash = Animator.StringToHash("throw");
     }
@@ -25,71 +26,14 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool forward = Input.GetKey("w");
-        bool right = Input.GetKey("d");
-        bool left = Input.GetKey("a");
-        bool backward = Input.GetKey("s");
+        var inAir = !Physics.CheckBox(groundCheck.position, groundCheck.localScale / 2f, groundCheck.rotation);
+        animator.SetBool(inAirHash, inAir);
 
-        bool jump = Input.GetKey(KeyCode.Space);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-        bool throwSnowball = Input.GetKey("f");
-
-        bool isRunning = animator.GetBool(isRunningHash);
-        bool isJumping = animator.GetBool(isJumpingHash);
-        bool inAir = animator.GetBool(inAirHash);
-        bool throwing = animator.GetBool(throwHash);
-
-
-
-        
-        if (throwSnowball && !throwing)
-        {
-            animator.SetBool(throwHash, true);
-        }
-        
-
-
-        if (forward && !isRunning)
-        {
-            animator.SetBool(isRunningHash, true);
-        }
-
-        if (!forward && isRunning)
-        {
-            animator.SetBool(isRunningHash, false);
-        }
-        
-        if (right)
-        {
-            animator.SetBool(isRunningHash, true);
-        }
-
-        if (left)
-        {
-            animator.SetBool(isRunningHash, true);
-        }
-
-        if (backward)
-        {
-            animator.SetBool(isRunningHash, true);
-        }
-
-        if((jump && isRunning) || (jump && !isRunning))
-        {
-            animator.SetBool(isJumpingHash, true);
-            animator.SetBool(inAirHash, true);
-        }
-
-        if(inAir && isJumping)
-        {
-            animator.SetBool(isJumpingHash, false);
-            animator.SetBool(inAirHash, false);
-        }
-        
-
-        
-
+        var running = direction.magnitude > 0.01f;
+        animator.SetBool(isRunningHash, running);
     }
-
-    
 }
--- Assets/Mizzoomers/Content/SnowballFight/Scripts/ThirdPersonMovement.cs
+++ Assets/Mizzoomers/Content/SnowballFight/Scripts/ThirdPersonMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
@@ -12,7 +13,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public float speed = 8f;
     public float jumpHeight = 3f;
 
-    Vector3 velocity;
+    [HideInInspector] public Vector3 velocity;
     public float gravity = -9.81f;
 
     // smooth out angles of rotation for player
@@ -30,6 +31,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public int maxHealth = 100;
 
     public HealthBar healthBar;
+    public CinemachineFreeLook freeLook;
 
     // Start is called before the first frame update
     void Start()
@@ -43,14 +45,14 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         // create a sphere at bottom of player to see if it hits the ground
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckBox(groundCheck.position, groundCheck.localScale / 2f, groundCheck.rotation);
 
         // check if on the ground
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-        
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
@@ -91,7 +93,19 @@ public class ThirdPersonMovement : MonoBehaviour
             Damage(10);
         }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        var camOn = Cursor.lockState == CursorLockMode.None ? 0 : 1;
+        freeLook.m_XAxis.m_MaxSpeed = camOn * 450;
+        freeLook.m_YAxis.m_MaxSpeed = camOn * 4;
     }
 
     public void Damage(int damage)
