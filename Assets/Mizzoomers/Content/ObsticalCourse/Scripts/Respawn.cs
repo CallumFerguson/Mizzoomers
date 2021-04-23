c79bcd77a85a8d95f6fd92d8f9a9d85de40d034d
jcmq6b <60238224+jcmq6b@users.noreply.github.com>
2021-04-23 16:20:49 -0500

    fine tuning objects

--- Assets/Mizzoomers/Content/ObsticalCourse/Scripts/Respawn.cs
+++ Assets/Mizzoomers/Content/ObsticalCourse/Scripts/Respawn.cs
@@ -6,6 +6,7 @@ public class Respawn : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Transform cam;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +16,7 @@ public class Respawn : MonoBehaviour
             Debug.Log("Respawning!");
             //player.transform.Rotate(180, 0, 0, Space.Self); //Why no work
             player.transform.position = respawnPoint.transform.position;
+            cam.transform.position = new Vector3(0, 11, -42);
         }
     }
 }
